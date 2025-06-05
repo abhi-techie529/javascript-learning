4b6edf5ee00df205b34fe4badef3925308c92b5e
abhi-techie <abhi-techie529@users.noreply.github.com>
2025-06-05 19:54:21 +0530

    problem 1

--- JS Questions/string/01_string.js
+++ JS Questions/string/01_string.js
@@ -1,5 +1,5 @@
 // try to replace "dog" with cat from sample string :- "He quick brown fox jumps over the lazy dog.if the dog barked ,was it really lazy ?"
 
-const story = "He quick brown fox jumps over the lazy dog.if the dog barked ,was it really lazy ?"
+const story = " He quick brown fox jumps over the lazy dog.if the dog barked ,was it really lazy ?"
 
 console.log(story.replaceAll("dog" , "cat"));
