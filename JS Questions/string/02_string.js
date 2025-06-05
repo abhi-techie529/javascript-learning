f7e7444fafb9ebc7480afb5a364b6881836dad0b
abhi-techie <abhi-techie529@users.noreply.github.com>
2025-06-05 20:00:42 +0530

    problem 2

--- JS Questions/string/02_string.js
+++ JS Questions/string/02_string.js
@@ -1,2 +1,13 @@
+// convert string to lowercase //
+
 const Case = "HE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.IF THE DOG BARKED ,WAS IT REALLY LAZY ?"
 console.log(Case.toLowerCase());
+
+
+// output :- he quick brown fox jumps over the lazy dog.if the dog barked ,was it really lazy ?
+
+
+
+// Problwm 3 , check "fox" is in the string or not
+
+console.log(Case.includes("FOX"));
